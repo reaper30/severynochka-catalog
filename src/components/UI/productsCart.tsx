909528d5c353c89ba5dafbc3a608ea02c5d20b12
Ilgiz <vurganowilgiz@gmail.com>
2025-10-13 00:09:39 +0400

    second

---
+++ src/components/UI/productsCart.tsx
@@ -0,0 +1,12 @@
+import { useProducts } from "@/hooks/useProducts";
+
+
+const Products = () => {
+	return ( <>
+		const {data} = useProducts({})
+		console.log(data);
+
+	</> );
+}
+
+export default Products;
--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -3,14 +3,19 @@ import { useQuery } from "@tanstack/react-query";
 
 
 	const getProducts = async () => {
-		const res = await axiosInstance.get()
+		const res = await axiosInstance.get(`/products`)
+		console.log(res);
+
+		return res
 	}
 
-const useProducts = (query) => {
+export const useProducts = () => {
 	const {data} = useQuery({
 		queryKey: ["products" ],
-		quryFn: () => getProducts(query),
+		quryFn: () => getProducts(),
 		initialData: []
 
 	})
+
+	return data
 }
